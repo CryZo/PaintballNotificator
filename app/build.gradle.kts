43f893942565c7d1c9e11b186d5a8e509fc6f5d0
Christian Haser <32584944+CryZo@users.noreply.github.com>
2024-03-28 14:37:53 +0100

    Minor improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.chaser.paintballnotificator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
